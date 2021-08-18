432f412552c2b98acb556dd8cf749f0d4bc005d3
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-08-18 23:05:17 +0200

    square waves

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,12 @@ fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).size(1920, 1080).build().unwrap();
     let mut points = Vec::new();
 
-    let radius = 1.0;
+    let radius = 20.0;
     let noise = Perlin::new();
-    for i in 0..360 {
-        for i2 in 0..10 {
-            let x = radius * deg_to_rad(i as f32 + 0.1 * i2 as f32).cos();
-            let y = radius * deg_to_rad(i as f32 + 0.1 * i2 as f32).sin();
-            points.push(vec2(x, y));
-        }
+    for _i in 0..400 {
+        let x = 0.0;
+        let y = 0.0;
+        points.push(vec2(x, y));
     }
 
     Model {
@@ -50,17 +48,23 @@ fn update(app: &App, model: &mut Model, _update: Update) {
     }
 
     if app.elapsed_frames() % 200 == 0 {
-        for i in 0..360 {
-            for i2 in 0..10 {
-                let x = model.radius * deg_to_rad(i as f32 + 0.1 * i2 as f32).cos();
-                let y = model.radius * deg_to_rad(i as f32 + 0.1 * i2 as f32).sin();
-                let point = model.points.get_mut(i*10 + i2).unwrap();
-                point.x = x;
-                point.y = y;
-            }
+        for i in 0..100 {
+            let val = (-model.radius / 2.0) + (i as f32 * model.radius / 100.0);
+            let max = model.radius / 2.0;
+
+            set_point(val, max, i, model);
+            set_point(max, -val, i+100, model);
+            set_point(-val, -max, i+200, model);
+            set_point(-max, val, i+300, model);
         }
         model.scale = random_range(0.0001, 0.01);
     }
+}
+
+fn set_point(x: f32, y: f32, i: usize, model: &mut Model) {
+    let point = model.points.get_mut(i).unwrap();
+    point.x = x;
+    point.y = y;
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -69,10 +73,9 @@ fn view(app: &App, model: &Model, frame: Frame) {
         draw.background().color(BLACK);
     }
 
-    let pointsize = 3.0;
-    for point in model.points.iter() {
-        draw.ellipse().x_y(point.x, point.y).w_h(pointsize, pointsize).color(WHITE);
-    }
+    let mut points = model.points.clone();
+    points.push(*points.get(0).unwrap());
+    draw.polyline().points(points).color(WHITE);
 
     draw.rect().w_h(2000.0, 2000.0).color(srgba(0.0, 0.0, 0.0, 0.1));
 
