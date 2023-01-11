083f05ec74680f8fb6befc8a45c1c12800f6d2ca
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-01-11 13:32:23 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use nannou::prelude::*;
+use nannou_audio as audio;
+use nannou_audio::Buffer;
+
+struct Audio {
+    sounds: Vec<audrey::read::BufFileReader>,
+}
 
 struct Model {
     points: Vec<Vec3>,
+    stream: audio::Stream<Audio>,
 }
 
 fn main() {
@@ -23,7 +30,57 @@ fn model(app: &App) -> Model {
         }
     }
 
-    Model { points }
+    // Initialise the audio host so we can spawn an audio stream.
+    let audio_host = audio::Host::new();
+
+    // Initialise the state that we want to live on the audio thread.
+    let sounds = vec![];
+    let model = Audio { sounds };
+    let stream = audio_host
+        .new_output_stream(model)
+        .render(audio)
+        .build()
+        .unwrap();
+
+    stream.play().unwrap();
+
+    let sound = audrey::open("Chemical.wav").expect("failed to load sound");
+    stream
+        .send(move |audio| {
+            audio.sounds.push(sound);
+        })
+        .ok();
+
+    Model { points, stream }
+}
+
+// A function that renders the given `Audio` to the given `Buffer`.
+// In this case we play the audio file.
+fn audio(audio: &mut Audio, buffer: &mut Buffer) {
+    let mut have_ended = vec![];
+    let len_frames = buffer.len_frames();
+
+    // Sum all of the sounds onto the buffer.
+    for (i, sound) in audio.sounds.iter_mut().enumerate() {
+        let mut frame_count = 0;
+        let file_frames = sound.frames::<[f32; 2]>().filter_map(Result::ok);
+        for (frame, file_frame) in buffer.frames_mut().zip(file_frames) {
+            for (sample, file_sample) in frame.iter_mut().zip(&file_frame) {
+                *sample += *file_sample;
+            }
+            frame_count += 1;
+        }
+
+        // If the sound yielded less samples than are in the buffer, it must have ended.
+        if frame_count < len_frames {
+            have_ended.push(i);
+        }
+    }
+
+    // Remove all sounds that have ended.
+    for i in have_ended.into_iter().rev() {
+        audio.sounds.remove(i);
+    }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
