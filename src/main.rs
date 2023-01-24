0d2725b082ba23a5c4e9898c8d8318d4dc957b97
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-01-24 02:37:14 +0100

    extremely flashy wave sphere (no noise)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use nannou::{prelude::*, math::ConvertAngle};
-use rand::prelude::*;
 
 struct Model {
     points: Vec<Vec3>,
@@ -61,7 +60,6 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
 
-    let mut rng = rand::thread_rng();
     let y_scale_time_mult = 4.0;
     let y_scale = map_range((y_scale_time_mult * app.time).sin(), -1.0, 1.0, 0.0, 1.0);
 
@@ -73,8 +71,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         let i = (i % 361) as f32;
         let periods = 20.0;
 
-        let rand_mult: f32 = rng.gen();
-        let wave_value = 2.0 * rand_mult * (i.deg_to_rad() * periods).sin();
+        let wave_value = 2.0 * (i.deg_to_rad() * periods).sin();
         rotate_x(point, app.time.sin());
         *point *= 4.0 * map_range(y_scale * wave_value, -1.0, 1.0, 1.0, 1.2) * (50.0 * app.time).sin();
 
