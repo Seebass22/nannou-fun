50e41a787cef46973c329dcb5a86afa21a028588
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-02-13 15:06:11 +0100

    3d random walk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use nannou::{prelude::*, math::ConvertAngle};
+use nannou::prelude::*;
+use rand::prelude::*;
 
 struct Model {
-    points: Vec<Vec3>,
+    old_location: Vec3,
+    location: Vec3,
 }
 
 fn main() {
@@ -10,46 +12,51 @@ fn main() {
 
 fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).size(1920, 1080).build().unwrap();
-    let mut points = Vec::new();
-
-    let radius = 1.0;
-    for theta in (0..=180).step_by(10) {
-        for phi in (0..=360).step_by(1) {
-            let theta = (theta as f32).deg_to_rad();
-            let phi = (phi as f32).deg_to_rad();
-            let x = radius * theta.sin() * phi.cos();
-            let y = radius * theta.sin() * phi.sin();
-            let z = radius * theta.cos();
-            points.push(vec3(x, y, z));
-        }
+    Model {
+        old_location: Vec3::ZERO,
+        location: Vec3::ZERO,
     }
-
-    Model { points }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    for point in model.points.iter_mut() {
-        rotate_x(point, 0.001);
-        rotate_y(point, 0.002);
-        rotate_z(point, 0.003);
+    model.old_location = model.location;
+    step(&mut model.location);
+}
+
+fn step(pos: &mut Vec3) {
+    let mut rng = thread_rng();
+    let rand: u8 = rng.gen();
+    let distance = 0.1;
+    let dir = if rand % 2 == 0 { 1.0 } else { -1.0 };
+    match rand % 3 {
+        0 => {
+            pos.x += dir * distance;
+        },
+        1 => {
+            pos.y += dir * distance;
+        },
+        2 => {
+            pos.z += dir * distance;
+        },
+        _ => { panic!() },
     }
 }
 
-fn rotate_z(point: &mut Vec3, angle: f32) {
+fn _rotate_z(point: &mut Vec3, angle: f32) {
     let s = angle.sin();
     let c = angle.cos();
     point.x = point.x * c - point.y * s;
     point.y = point.x * s + point.y * c;
 }
 
-fn rotate_x(point: &mut Vec3, angle: f32) {
+fn _rotate_x(point: &mut Vec3, angle: f32) {
     let s = angle.sin();
     let c = angle.cos();
     point.y = point.y * c - point.z * s;
     point.z = point.y * s + point.z * c;
 }
 
-fn rotate_y(point: &mut Vec3, angle: f32) {
+fn _rotate_y(point: &mut Vec3, angle: f32) {
     let s = angle.sin();
     let c = angle.cos();
     point.x = point.x * c - point.z * s;
@@ -58,47 +65,23 @@ fn rotate_y(point: &mut Vec3, angle: f32) {
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(BLACK);
+    if app.elapsed_frames() == 0 {
+        draw.background().color(BLACK);
+    }
 
-    let mut points: Vec<(Vec<Vec2>, Vec<Vec3>)> = Vec::new();
-    let mut points_vec = Vec::new();
-    let mut colors_vec = Vec::new();
-
-    let mut ring_i = 0;
-    for (i, point) in model.points.clone().iter_mut().enumerate() {
-
-        let i = (i % 361) as f32;
-        let periods = map_range((0.5 * app.time).sin().abs(), 0.0, 1.0, 0.0, 20.0);
-        let ring_mult = map_range((ring_i as f32 * 0.9 + 12.0 * app.time).sin(), -1.0, 1.0, 1.0, 1.5);
-
-        let wave_value = 2.0 * (i.deg_to_rad() * periods).sin();
-        rotate_x(point, 0.8 * app.time.sin());
-
-        *point *= 4.0 * map_range(wave_value, -2.0, 2.0, 0.8, 1.2);
-        *point *= ring_mult;
-
+    let mut line_points: [Vec2; 2] = [Vec2::ZERO; 2];
+    let mut line_color_points: [Vec3; 2] = [Vec3::ZERO; 2];
+    for (i, point) in [model.old_location, model.location].iter().enumerate() {
         let z = point.z - 10.0;
         let x = point.x / (0.01 * z);
         let y = point.y / (0.01 * z);
-
-        points_vec.push(Vec2::new(10.0 * x, 10.0 * y));
-        colors_vec.push(*point);
-        if points_vec.len() == 361 {
-            points.push((points_vec.clone(), colors_vec.clone()));
-            points_vec.clear();
-            colors_vec.clear();
-            ring_i += 1;
-        }
+        line_points[i] = Vec2::new(10.0 * x, 10.0 * y);
+        line_color_points[i] = *point;
     }
 
-    for (i, (points_vec, colors_vec)) in points.into_iter().enumerate() {
-        let r = map_range((i as f32 * 0.9 + 12.0 * app.time).sin(), -1.0, 1.0, 0.0, 1.0);
-        let b = map_range(colors_vec[0].z, -4.0, 0.1, 0.0, 1.0);
-        if r > 0.1 {
-            draw.polyline().weight(2.0).points(points_vec)
-                .color(srgb(r, 0.0, b));
-        }
-    }
+    let r = 1.0;
+    draw.polyline().weight(2.0).points(line_points)
+        .color(srgb(r, 0.0, 0.0));
 
     draw.to_frame(app, &frame).unwrap();
 }
