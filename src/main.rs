37441497947b574152ab390850e6e2fecb750d11
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2024-09-01 23:08:39 +0200

    random squares

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use nannou::{
-    ease::{self, map_clamp},
     prelude::*,
+    rand::{thread_rng, Rng},
 };
 
 struct Model {
-    add: f32,
+    indices: [u8; 16],
 }
 
 fn main() {
@@ -19,28 +19,43 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    Model { add: 0.0 }
+    Model { indices: [0u8; 16] }
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {}
+fn update(app: &App, model: &mut Model, _update: Update) {
+    if app.elapsed_frames() % 10 == 0 {
+        let mut rng = thread_rng();
+        let mut indices = [0u8; 16];
+        for i in indices.iter_mut() {
+            *i = rng.gen_range(0..144);
+        }
+        model.indices = indices;
+    }
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
 
-    let mut i = app.time * 128.0;
+    let mut i = 0.0;
+    let index_add = app.time * 128.0;
+
     let width = 160.0;
 
     for y in 0..9 {
         for x in 0..16 {
             let x = x as f32 * width - 1280.0 + 0.5 * width;
             let y = y as f32 * width - 720.0 + 0.5 * width;
-            let index = fmod(i, 144.0);
+            let index = fmod(i + index_add, 144.0);
 
+            let mut add = 0.0;
+            if model.indices.contains(&(i as u8)) {
+                add = 1.0;
+            }
             draw.rect().x_y(x, y).w_h(width, width).color(LinSrgb::new(
-                model.add + index / 144.0,
-                model.add + index / 144.0,
-                model.add + index / 144.0,
+                add + index / 144.0,
+                0.5 * add + index / 144.0,
+                0.1 * add + index / 144.0,
             ));
             i += 1.0;
         }
